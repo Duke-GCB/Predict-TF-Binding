d8986b76d7d6312683247907e74f9681acb5cc32
Dan Leehr <dan.leehr@duke.edu>
2016-02-12 11:02:11 -0500

    Test case for feature matrix

---
+++ test_predict_genome.py
@@ -0,0 +1,35 @@
+import unittest
+
+from predict_genome import svr_features_from_sequence
+
+class TestPredictGenome(unittest.TestCase):
+
+    def test_svr_features_from_sequence_1mer(self):
+        sequence = 'AGC'
+        features = svr_features_from_sequence(sequence, [1])
+        self.assertEqual(len(features), 12, "Feature matrix size mismatch") # with 4 nucleotides and 3 positions there should be 12 features
+        just_matches = filter(lambda x: x['value'] == 1, features)
+        self.assertEqual(len(just_matches), 3, "Features of value 1 size mismatch") # For 1mers, there should only be 3 items with value 1
+        positions = map(lambda x: x['position'], just_matches)
+        just_nucleotides = map(lambda x: x['feature'], just_matches) # Extract the original sequence
+        self.assertEqual(''.join(just_nucleotides), sequence, "Reconstructed sequence does not match")
+
+    def test_svr_features_from_sequence_1_2_3mer(self):
+        sequence = 'CAGGCTTTGGGAGCCAGCGGGGCGGGAGCGGCGAAG'
+        features = svr_features_from_sequence(sequence, [1,2,3])
+         # with 4 nucleotides and 1,2,3 positions, total should be 4^1*36 + 4^2 * 35 + 4^3 * 34 = 2880
+        self.assertEqual(len(features), 2880, "Feature matrix size mismatch")
+
+        just_matches = filter(lambda x: x['value'] == 1, features)
+        print len(just_matches)
+        # Should be 36+35+34 = 105 items where value is 1
+        self.assertEqual(len(just_matches), 105, "Features of value 1 size mismatch")
+        positions = map(lambda x: x['position'], just_matches)
+
+        # Extract the original sequence, just the 1mers
+        just_nucleotides = map(lambda x: x['feature'], just_matches[0:36])
+        self.assertEqual(''.join(just_nucleotides), sequence, "Reconstructed sequence does not match")
+
+
+if __name__ == '__main__':
+    unittest.main()
